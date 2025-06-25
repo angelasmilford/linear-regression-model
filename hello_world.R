4dc08d506975446a88d362ba73b0f04351a50a5d
angelasmilford <angela.milford123@gmail.com>
2025-06-25 02:59:12 -1100

    First R program

---
+++ hello_world.R
@@ -0,0 +1,18 @@
+print("Hello, World!")
+
+print(2)
+print(4)
+print(6)
+print(11)
+
+print(5 + 5)
+
+plot(1:4)
+
+# This is a comment
+# R does not have a syntax for multi-line comments, so
+# for we can just insert a # to create multi-line comments
+
+# When outputting code in R, the "print()" function doesn't have to be used
+# However, for VS code, it is required to use "print()" to see the output in the console
+# When outputting the result inside an R expression (inside curly braces), the print() function must be used
